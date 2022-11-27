86daf2f28a6724544e0b6f8e61b188250c184a48
welling <wellingfine@qq.com>
2022-11-28 01:06:57 +0800

    delete print

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -3,7 +3,6 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
 import './styles/index.scss'
-import './permission'
 import App from './App.vue'
 import router from './router'
 import { errorHandler } from './error'
--- src/renderer/router/constantRouterMap.ts
+++ src/renderer/router/constantRouterMap.ts
@@ -1,9 +1,8 @@
 import { RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
-    { path: '/:pathMatch(.*)*', component: () => import("@renderer/views/404.vue") },
-    { path: '/', name: '总览', component: () => import('@renderer/components/LandingPage.vue') },
-    { path: '/Print', name: '打印', component: () => import('@renderer/views/Print.vue') },
+  // { path: '/:pathMatch(.*)*', component: () => import("@renderer/views/404.vue") },
+  { path: '/', name: '总览', component: () => import('@renderer/components/LandingPage.vue') },
 ]
 
 export default routes
