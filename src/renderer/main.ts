484022df98c1aaa588ac9e27b05bd18b3ea7418d
welling <wellingfine@qq.com>
2022-11-29 23:46:23 +0800

    update

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
-import './styles/index.scss'
 import App from './App.vue'
 import router from './router'
 import { errorHandler } from './error'
